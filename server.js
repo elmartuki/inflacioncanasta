70b93d4bf7ab2ffa5a9155a866b3500ac7c152db
martuki <33644103+elmartuki@users.noreply.github.com>
2025-07-17 17:35:18 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
   "https://inflacioncanasta.ddns.net",
+  "https://inflacioncanasta.vercel.app",
   "https://localhost:4000",
 ];
 
